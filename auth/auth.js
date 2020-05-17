7f3eb7c02d783257aa5bc3e875e25aa260c3bff8
kbpsystem <koleen.bp@outlook.com>
2020-05-17 23:49:37 +0800

    building auth logic

---
+++ auth/auth.js
@@ -0,0 +1,54 @@
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const UserModel = require("../model/model");
+
+// passport middleware for registration
+passport.use(
+  "signup",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        // save the info from the user to DB
+        const user = await UserModel.create({ email, password });
+        // send user info to middleware
+        return done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+// create passport middleware to handle user login
+passport.use(
+  "login",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        // find the user associated with the email
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+          // if email not found
+          return done(null, false, { message: "User not found" });
+        }
+        // validate password if it matches on the DB
+        const validate = await user.isValidPassword(password);
+        if (!isvalidate) {
+          return done(null, false, { message: "Wrong password" });
+        }
+        // send the user info to next middleware
+        return done(null, user, { message: "Logged in successfully!" });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
