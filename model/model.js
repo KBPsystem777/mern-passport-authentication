f1c775f2f3712b61b0963d4d1bb25199cf6b2288
kbpsystem <koleen.bp@outlook.com>
2020-05-17 23:49:49 +0800

    building usermodel

---
+++ model/model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// hashing pasword
+UserSchema.pre("save", async function (next) {
+  const user = this;
+  // hashing
+  const hash = await bcrypt.hash(this.password, 10);
+  // replacing the plain password with the encrypted one
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
+
+const UserModel = mongoose.model("user", UserSchema);
+
+module.exports = UserModel;
