b6e1fe029d19d7a6933ddda3060f8fd51a7ea82f
kbpsystem <koleen.bp@outlook.com>
2020-05-17 23:49:22 +0800

    building signup route

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
+
+router.post(
+  "/signup",
+  passport.authenticate("signup", {
+    session: false,
+  }),
+  async (req, res, next) => {
+    res.json({
+      message: "Success!",
+      user: req.user,
+    });
+  }
+);
