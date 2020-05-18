426bb15df12ebc5e6473bb6e23a3711fb66d01f3
kbpsystem <koleen.bp@outlook.com>
2020-05-18 13:10:01 +0800

    rebuiding server logic

--- Server.js
+++ Server.js
@@ -1,20 +1,41 @@
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const mongoose = require("mongoose");
-const errorHandler = require("errorhandler");
-
-// configure mongoose's promise to global
-mongoose.promise = global.Promise;
-
-// isProduction?
-const isProduction = process.env.NODE_ENV === "production";
-
+const bodyParser = require("body-parser");
+const passport = require("passport");
 const app = express();
-app.use(cors());
-app.use(require("morgan")("dev"));
+const UserModel = require("./model/model");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/passport-jwt", {
+    keepAlive: 1,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log(new Date() + "Mongoose connected!"));
+mongoose.connection.on("error", (error) => console.log(new Date() + error));
+mongoose.Promise = global.Promise;
+
+require("./auth/auth");
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(session({ secret: "KBPsecretkey", cookie: { max } }));
+
+const routes = require("./routes/routes");
+const securedRoutes = require("./routes/secured-routes");
+
+app.use("/", routes);
+// securing '/user' route
+app.use(
+  "/user",
+  passport.authenticate("jwt", { session: false }),
+  securedRoutes
+);
+
+// handling errors
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
+
+app.listen(3000, () => {
+  console.log(new Date() + "Server Started!");
+});
