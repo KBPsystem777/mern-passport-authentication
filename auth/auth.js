875e3d56b6dd7ab4a44fe2cd391b67b8ce2499fd
kbpsystem <koleen.bp@outlook.com>
2020-05-20 23:52:41 +0800

    fixed passport login config

--- auth/auth.js
+++ auth/auth.js
@@ -33,19 +33,20 @@ passport.use(
     },
     async (email, password, done) => {
       try {
-        // find the user associated with the email
+        //Find the user associated with the email provided by the user
         const user = await UserModel.findOne({ email });
         if (!user) {
-          // if email not found
+          //If the user isn't found in the database, return a message
           return done(null, false, { message: "User not found" });
         }
-        // validate password if it matches on the DB
+        //Validate password and make sure it matches with the corresponding hash stored in the database
+        //If the passwords match, it returns a value of true.
         const validate = await user.isValidPassword(password);
-        if (!isvalidate) {
-          return done(null, false, { message: "Wrong password" });
+        if (!validate) {
+          return done(null, false, { message: "Wrong Password" });
         }
-        // send the user info to next middleware
-        return done(null, user, { message: "Logged in successfully!" });
+        //Send the user information to the next middleware
+        return done(null, user, { message: "Logged in Successfully" });
       } catch (error) {
         return done(error);
       }
