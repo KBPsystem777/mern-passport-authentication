0eb02067da86380bfea4aa811c6a8305630e43e4
kbpsystem <koleen.bp@outlook.com>
2020-05-24 00:40:50 +0800

    added GET all users response

--- routes/secured-routes.js
+++ routes/secured-routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../model/model");
 // route below should be accessed only by authorized clients
 
+// secured page for "/profile"
 router.get("/profile", (req, res, next) => {
   // send back the user details and token
   res.json({
@@ -12,4 +13,23 @@ router.get("/profile", (req, res, next) => {
   });
 });
 
+// secured page for getting all users
+router.get("/users", (req, res, next) => {
+  User.find()
+    .then((users) =>
+      res.json({
+        status: "SUCCESS",
+        action: "GET USERS",
+        date: new Date(),
+        data: users,
+      })
+    )
+    .catch((err) =>
+      res.status(400).json({
+        status: "FAILED-ERROR",
+        action: "GET USERS",
+        description: `Error: ${err}`,
+      })
+    );
+});
 module.exports = router;
